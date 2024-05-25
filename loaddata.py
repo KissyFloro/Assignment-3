45cddc0240a330ba69e7272f5c14e26856ab7a16
Kissy Eve Amor Floro <167738202+KissyFloro@users.noreply.github.com>
2024-05-25 21:27:43 +0800

    Update loaddata.py

--- loaddata.py
+++ loaddata.py
@@ -1,18 +1,18 @@
 #  Load data from files using numpy
 
 # Example 1: load single array in .npy file
-# import numpy as np
-# data = np.load('array1.npy')
-# print('Loaded Array:', data)
+import numpy as np
+data = np.load('array1.npy')
+print('Loaded Array:', data)
  
 # Example 2: load multiple array in .npz file
-# import numpy as np
-# data = np.load('arrays.npz')
-# a1 = data['a1']
-# a2 = data['a2']
+import numpy as np
+data = np.load('arrays.npz')
+a1 = data['a1']
+a2 = data['a2']
 
-# print('Array 1:', a1)
-# print('Array 2:', a2)
+print('Array 1:', a1)
+print('Array 2:', a2)
 
 # # # Example 3: load data from text file
 import numpy as np
